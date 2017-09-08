72225c30d41106a385cd2bb01606944d59ad8528
RuioWolf <lytjiong@gmail.com>
2017-09-08 13:07:44 +0800

    Basic Creat and Check

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using static WindowsFormsApp2.ConfigRw;
+
+namespace WindowsFormsApp2
+{
+	public partial class Form1 : Form
+	{
+		//Init
+		string cfg = Environment.CurrentDirectory + "\\config.xml";
+
+		bool isxmlok;
+
+		ConfigRw c=new ConfigRw();
+
+		public Form1()
+		{
+			ConfigRw.Init();
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			ConfigRw.FirstInit();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			if (CheckXml())
+			{
+				MessageBox.Show("Verify Success");
+			}
+			else
+			{
+				MessageBox.Show("XML Error");
+			}
+		}
+
+		//        public static void main(String[] args)
+		//        {
+		//            
+		//        }
+	}
+}
